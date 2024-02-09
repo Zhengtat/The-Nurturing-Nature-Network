f27eaee3a9fce949e52490bc1fe06f7ce5f56ff6
Zhengtat <124166333+Zhengtat@users.noreply.github.com>
2024-02-10 07:56:35 +0800

    Delete Hack4good/models directory

--- Hack4good/models/Location.js
+++
@@ -1,9 +0,0 @@
-// models/Location.js
-const mongoose = require('mongoose');
-
-const locationSchema = new mongoose.Schema({
-  name: String,
-  // Other location details
-});
-
-module.exports = mongoose.model('Location', locationSchema);
